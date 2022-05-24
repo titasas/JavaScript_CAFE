1d57cc996e8705d35fecda57c32cc2353974d961
titasas <titas.petrauskas@gmail.com>
2022-05-24 22:01:18 +0300

    15 Lesson

---
+++ 15 Lesson/script.js
@@ -0,0 +1,69 @@
+//TASK ONE
+// function hello() {
+//   console.log("Hello World!");
+// }
+
+// hello();
+
+// TASK TWO
+// function getHelloString() {
+//   return "Hello World!";
+// }
+
+// console.log(getHelloString());
+
+// TASK THREE
+// function sum(num1, num2, num3) {
+//   return num1 + num2 + num3;
+// }
+
+// console.log(sum(10, 20, 30));
+
+//TASK FOUR
+// function count(num) {
+//   for (let i = 1; i <= num; i++) {
+//     console.log(i);
+//   }
+// }
+// count(14);
+
+//TASK FIVE
+// function isAdult(age) {
+//   const years = 18 - age;
+//   if (age < 17) {
+//     return `the person is ${age} years old, after ${years} years they will be an adult`;
+//   } else {
+//     return `the person is ${age} years old and they are an adult`;
+//   }
+// }
+
+// console.log(isAdult(15));
+
+//TASK SEVEN
+// function calculateArea(width, height) {
+//   const area = width * height;
+//   return area.toFixed(2);
+// }
+
+// console.log(calculateArea(5, 6));
+
+//TASK EIGHT
+// function sum(limit) {
+//   let sum = 0;
+//   for (let i = 1; i <= limit; i++) {
+//     sum += i;
+//   }
+//   return sum;
+// }
+
+// console.log(sum(6));
+
+//TASK NINE
+// function reverseString(str) {
+//   const stringArray = str.split("");
+//   for (let i = 0; i < str.length; i++) {
+//     stringArray.reverse(i, str.length - (i + 1));
+//   }
+//   return stringArray.join("");
+// }
+// console.log(reverseString("Titas"));
