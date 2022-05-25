f4483b0ad4f3aaaba2b8fd4723ad33cd8ad472a1
titasas <titas.petrauskas@gmail.com>
2022-05-25 21:56:16 +0300

    16 Lesson plus home tasks

---
+++ CA Online/03 homework May 25/script.js
@@ -0,0 +1,29 @@
+//EXERCISE ONE
+// function sum(a, b, c) {
+//   return a + b + c;
+// }
+// console.log(sum(1, 2, 3));
+
+//EXERCISE TWO
+// function printName() {
+//   const userName = prompt("What's your name?");
+//   return userName;
+// }
+
+// console.log(printName());
+
+//EXERCISE THREE - need help
+// const myArray = [
+//   {
+//     housing: "flat",
+//     rooms: 2,
+//     yearBuilt: 2000,
+//     isRented: true,
+//   },
+// ];
+
+// function print(x) {
+//   console.log(`${key} ${x}`);
+// }
+
+// forEach(myArray, print(key));
