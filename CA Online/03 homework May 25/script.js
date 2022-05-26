c34636748acffebd201b1a42d6029b6e2e412a76
titasas <titas.petrauskas@gmail.com>
2022-05-26 18:07:55 +0300

    homework exercise 3

--- CA Online/03 homework May 25/script.js
+++ CA Online/03 homework May 25/script.js
@@ -12,18 +12,48 @@
 
 // console.log(printName());
 
-//EXERCISE THREE - need help
-// const myArray = [
+//EXERCISE THREE
+// const cars = [
 //   {
-//     housing: "flat",
-//     rooms: 2,
-//     yearBuilt: 2000,
-//     isRented: true,
+//     brand: "BMW",
+//     year: 2000,
+//     originalPrice: 10000,
+//     priceNow: function () {
+//       return this.originalPrice - (2022 - this.year) * 300;
+//     },
+//   },
+//   {
+//     brand: "OPEL",
+//     year: 2007,
+//     originalPrice: 8000,
+//     priceNow: function () {
+//       return this.originalPrice - (2022 - this.year) * 300;
+//     },
+//   },
+//   {
+//     brand: "SEAT",
+//     year: 2010,
+//     originalPrice: 12000,
+//     priceNow: function () {
+//       return this.originalPrice - (2022 - this.year) * 300;
+//     },
+//   },
+//   {
+//     brand: "VOLKSWAGEN",
+//     year: 2017,
+//     originalPrice: 28000,
+//     priceNow: function () {
+//       return this.originalPrice - (2022 - this.year) * 300;
+//     },
 //   },
 // ];
 
-// function print(x) {
-//   console.log(`${key} ${x}`);
-// }
+// cars.forEach((car) =>
+//   console.log(
+//     `The original price of this ${car.brand} in ${car.year} was ${
+//       car.originalPrice
+//     } money units, and now the car costs ${car.priceNow()}`
+//   )
+// );
 
-// forEach(myArray, print(key));
+//EXERCISE FOUR
