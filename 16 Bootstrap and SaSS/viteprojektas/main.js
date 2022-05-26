040ce4c91b1dbdc740aa36dd6639a5b8b6155073
titasas <titas.petrauskas@gmail.com>
2022-05-26 22:11:14 +0300

    17 Lesson

--- 16 Bootstrap and SaSS/viteprojektas/main.js
+++ 16 Bootstrap and SaSS/viteprojektas/main.js
@@ -1,6 +1,2 @@
 import "./style.scss";
-
-document.querySelector("#app").innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+import "..node_modules/bootstrap/dist/css/bootstrap-grid-min.css";
